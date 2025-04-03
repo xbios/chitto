66dab893a544ba2a7918f8c2477e95ecc98de730
EROL KILBÜKER <erolposta@gmail.com>
2025-04-04 00:16:55 +0300

    update database settings to use MySQL and add Product model with initial migration

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255) # Ürün adı 
+    description = models.TextField() # Ürün açıklaması
+    url = models.CharField(max_length=255) # Ürün resmi
+    slug = models.SlugField(max_length=255) # Ürün slug'ı
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Ürün fiyatı
+    date = models.DateField() # Ürün tarihi
+    isActive = models.BooleanField(default=True) # Ürün aktif mi?
+    isUpdated = models.BooleanField(default=False) # Ürün güncellendi mi
+    
