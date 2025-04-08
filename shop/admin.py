2590151a29d1953779aab76e16aa175a997e87fa
EROL KILBÜKER <erolposta@gmail.com>
2025-04-08 23:14:36 +0300

    add Category model and migration; update admin panel and navigation layout

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.site_header = "Chitto Admin Paneli"
+admin.site.site_title = "Chitto Admin Paneli"
+admin.site.index_title = "Chitto Admin Paneli" # Admin panelinin başlığı
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- shop/models.py
+++ shop/models.py
@@ -10,4 +10,14 @@ class Product(models.Model):
     date = models.DateField() # Ürün tarihi
     isActive = models.BooleanField(default=True) # Ürün aktif mi?
     isUpdated = models.BooleanField(default=False) # Ürün güncellendi mi
-    
+
+    def __str__(self):
+        return f"{self.name} -  {self.price} TL"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"    
